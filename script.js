a8406421bf1d1714c1ce272c4f777f4c47453b12
MthBarber <mthbarber@gmail.com>
2022-09-08 14:47:39 +0100

    render current chosen location to frontend

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Function that directs uses the OpenWeatherMap API
 async function getWeather(location) {
     try {
-        const info = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&APPID=HIDE_KEY`, { mode: 'cors' })        
+        const info = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&APPID=fefaae1901088ebdb24add3e1679ecb0`, { mode: 'cors' })
         let convertedInfo = await info.json()
         console.log(convertedInfo)
         return convertedInfo
@@ -13,20 +13,49 @@ async function getWeather(location) {
 locationWeather("London")
 
 //Function that calls getWeather and stores the result in an object to be converted onto the html page
-async function locationWeather(location){
-    let locationName = await getWeather(location);
-    let currentLocation = {
-        humidity : locationName.main.humidity,
-        name : locationName.name,
-        temperature : locationName.main.temp,
-        tempMax : locationName.main.temp_max,
-        tempMin : locationName.main.temp_min,
-        time : locationName.timezone,
-        weatherDescription: locationName.weather[0].description,
-        weatherIcon : locationName.weather[0].icon,
-        weatherName: locationName.weather[0].main,        
-        wind: locationName.wind.speed,
+async function locationWeather(location) {
+    try {
+        let locationName = await getWeather(location);
+        let currentLocation = {
+            humidity: locationName.main.humidity,
+            name: locationName.name,
+            temperature: locationName.main.temp,
+            tempMax: locationName.main.temp_max,
+            tempMin: locationName.main.temp_min,
+            time: locationName.timezone,
+            weatherDescription: locationName.weather[0].description,
+            weatherIcon: locationName.weather[0].icon,
+            weatherName: locationName.weather[0].main,
+            wind: locationName.wind.speed,
+        }
+        
+        document.getElementById('locationName').innerHTML = `${currentLocation.name}`
+        document.getElementById('currentTemp').innerHTML = `${currentLocation.temperature}°C`        
+        document.getElementById('weatherIcon').innerHTML = `Symbol here ${currentLocation.weatherIcon}`
+        document.getElementById('weatherName').innerHTML = `${currentLocation.weatherName}`
+        document.getElementById('humidity').innerHTML = `${currentLocation.humidity}%`
+        document.getElementById('wind').innerHTML = `${currentLocation.wind * 3.6} KM/H`
+        document.getElementById('tempMax').innerHTML = `${currentLocation.tempMax}°C`
+        document.getElementById('tempMin').innerHTML = `${currentLocation.tempMin}°C`
+
+    } catch {
+        console.log("Failed to make an object")
     }
-    console.log("Current temperature is", currentLocation.temperature)
-    console.table(currentLocation)
+
+
+
 }
+
+//Create function for users to search for requested location's weather
+
+let form = document.querySelector('form')
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    console.log(searchBar.value)
+    locationWeather(searchBar.value);
+})
+
+//Set the date
+const date = new Date();
+document.getElementById('date').innerHTML = date.toDateString()
