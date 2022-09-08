e02d95e8a322285f053de9572f6e605cda40f700
MthBarber <mthbarber@gmail.com>
2022-09-08 11:09:31 +0100

    add function that stores JSON returned from API call into an object

--- script.js
+++ script.js
@@ -1,11 +1,32 @@
+// Function that directs uses the OpenWeatherMap API
 async function getWeather(location) {
     try {
-        const info = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=HIDE_KEY`, { mode: 'cors' })        
+        const info = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&APPID=HIDE_KEY`, { mode: 'cors' })        
         let convertedInfo = await info.json()
         console.log(convertedInfo)
+        return convertedInfo
     } catch (error) {
         console.log("Whoops! made an error", error)
     }
 }
 
-getWeather("London")
+locationWeather("London")
+
+//Function that calls getWeather and stores the result in an object to be converted onto the html page
+async function locationWeather(location){
+    let locationName = await getWeather(location);
+    let currentLocation = {
+        humidity : locationName.main.humidity,
+        name : locationName.name,
+        temperature : locationName.main.temp,
+        tempMax : locationName.main.temp_max,
+        tempMin : locationName.main.temp_min,
+        time : locationName.timezone,
+        weatherDescription: locationName.weather[0].description,
+        weatherIcon : locationName.weather[0].icon,
+        weatherName: locationName.weather[0].main,        
+        wind: locationName.wind.speed,
+    }
+    console.log("Current temperature is", currentLocation.temperature)
+    console.table(currentLocation)
+}
