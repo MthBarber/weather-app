66fda03d621ef710c2cfbb7764c87c9b30e32f6b
MthBarber <mthbarber@gmail.com>
2022-09-08 10:12:24 +0100

    Fetch request to OpenWeather API all working

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-async function getWeather(location){
-    try{
-    const info = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=HIDE_KEY_`);    
-    console.log(info)
-    }catch (error) {
+async function getWeather(location) {
+    try {
+        const info = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=HIDE_KEY`, { mode: 'cors' })        
+        let convertedInfo = await info.json()
+        console.log(convertedInfo)
+    } catch (error) {
         console.log("Whoops! made an error", error)
     }
 }
